9e9fca09d3337b1681e0b374d9a3dabcb83b0af1
Fidelia Mallister <fmallist@oa-n5.msk.21-school.ru>
2020-07-31 11:25:43 +0300

    main change

--- exercise/correct_task/main.c
+++ exercise/correct_task/main.c
@@ -4,9 +4,6 @@ int aplusb(int a, int b);
 
 int main(void)
 {
-	if (aplusb(3, 1) != 4)
-		printf("ERROR!\nEXECTED:4\nSTUDENTS:%d\n", aplusb(3, 1));
-	else
-		printf("TEST 1...OK:)");
+	printf("%d\n%d\n", aplusb(1, 2), aplusb(3, 4));
 	return (0);
 }
